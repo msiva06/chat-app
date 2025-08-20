d9dcc8484dc103aa44eb4e532368383e5894a298
msiva06 <m.sivasubramanian06@gmail.com>
2025-08-20 10:38:52 -0500

    Websocket configuration

---
+++ back-end/src/main/java/com/example/messaging/websocket/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.example.messaging.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat-websocket").setAllowedOrigins("http://localhost:3000");
+                ;
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> list) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        list.add(converter);
+        return true;
+    }
+
+}
